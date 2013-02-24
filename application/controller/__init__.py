c6b5414378fa07974a84ddaf4cd9db5a642d70ae
@damnwidget <oscar.campos@member.fsf.org>
2013-02-24 20:34:22 +0000

    added global template params

--- application/controller/__init__.py
+++ application/controller/__init__.py
@@ -1,1 +1,25 @@
 # Controllers should be placed here
+
+template_args = {
+    'menu': True,
+    'menu_options': [
+        {'path': '/', 'label': 'Home', 'active': False, 'available': True},
+        {
+            'path': 'gettingstart', 'label': 'Get started',
+            'active': False, 'available': True
+        },
+        {
+            'path': 'docs', 'label': 'Documentation',
+            'active': False, 'available': True
+        },
+        {
+            'path': 'download', 'label': 'Download',
+            'active': False, 'available': False
+        },
+        {'path': 'blog', 'label': 'Blog', 'active': False, 'available': True},
+        {
+            'path': 'contact', 'label': 'Contact',
+            'active': True, 'available': True
+        },
+    ]
+}
